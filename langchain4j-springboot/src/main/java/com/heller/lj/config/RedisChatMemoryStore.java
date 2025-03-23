19f280fb94e1694deb7c8d684f7fd3ac2b9c5c87
Castlebin <Castlebin@gmail.com>
2025-03-23 11:32:05 +0800

    使用 Redis 来存储对话的上下文

--- langchain4j-springboot/src/main/java/com/heller/lj/config/RedisChatMemoryStore.java
+++ langchain4j-springboot/src/main/java/com/heller/lj/config/RedisChatMemoryStore.java
@@ -5,14 +5,14 @@ import java.util.List;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.ChatMessageDeserializer;
+import dev.langchain4j.data.message.ChatMessageSerializer;
 import dev.langchain4j.store.memory.chat.ChatMemoryStore;
 
 /**
  * 使用 Redis 来存储对话的上下文
- *
+ * <p>
  * 可以储存到任意地方，需要实现 ChatMemoryStore 接口
- *
- * TODO 实现具体功能
  */
 public class RedisChatMemoryStore implements ChatMemoryStore {
 
@@ -24,18 +24,20 @@ public class RedisChatMemoryStore implements ChatMemoryStore {
 
     @Override
     public List<ChatMessage> getMessages(Object memoryId) {
-        // TODO
-        return null;
+        String json = (String) redisTemplate.opsForValue().get(memoryId);
+        List<ChatMessage> chatMessages = ChatMessageDeserializer.messagesFromJson(json);
+        return chatMessages;
     }
 
     @Override
     public void updateMessages(Object memoryId, List<ChatMessage> list) {
-        // TODO
+        String json = ChatMessageSerializer.messagesToJson(list);
+        redisTemplate.opsForValue().set(memoryId, json);
     }
 
     @Override
     public void deleteMessages(Object memoryId) {
-        // TODO
+        redisTemplate.delete(memoryId);
     }
 
 }
