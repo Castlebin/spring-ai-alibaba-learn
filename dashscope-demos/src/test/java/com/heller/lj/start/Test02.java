9bd4b552b6569feb10ecd0a31b53baf9aa8d2e6a
Castlebin <Castlebin@gmail.com>
2025-04-12 23:49:02 +0800

    使用 DashScope SDK 进行调用

---
+++ dashscope-demos/src/test/java/com/heller/lj/start/Test02.java
@@ -0,0 +1,54 @@
+package com.heller.lj.start;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import com.alibaba.dashscope.aigc.generation.Generation;
+import com.alibaba.dashscope.aigc.generation.GenerationParam;
+import com.alibaba.dashscope.aigc.generation.GenerationResult;
+import com.alibaba.dashscope.common.Message;
+import com.alibaba.dashscope.common.Role;
+import com.alibaba.dashscope.exception.ApiException;
+import com.alibaba.dashscope.exception.InputRequiredException;
+import com.alibaba.dashscope.exception.NoApiKeyException;
+import com.alibaba.dashscope.utils.JsonUtils;
+
+public class Test02 {
+    public static GenerationResult callWithMessage() throws ApiException, NoApiKeyException, InputRequiredException {
+        Generation gen = new Generation();
+        Message systemMsg = Message.builder()
+                .role(Role.SYSTEM.getValue())
+                .content("You are a helpful assistant.")
+                .build();
+        Message userMsg = Message.builder()
+                .role(Role.USER.getValue())
+                .content("你是谁？")
+                .build();
+        GenerationParam param = GenerationParam.builder()
+                // 若没有配置环境变量，请用百炼 API Key 将下行替换为：.apiKey("sk-xxx")
+                .apiKey(System.getenv("DASHSCOPE_API_KEY"))
+                // 此处以 qwen-plus 为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
+                .model("qwen-plus")
+                .messages(Arrays.asList(systemMsg, userMsg))
+                .resultFormat(GenerationParam.ResultFormat.MESSAGE)
+                .build();
+        return gen.call(param);
+    }
+
+    /**
+     * 使用 DashScope SDK 进行调用
+     */
+    @Test
+    void test_DashScope_SDK_01() {
+        try {
+            GenerationResult result = callWithMessage();
+            System.out.println(JsonUtils.toJson(result));
+        } catch (ApiException | NoApiKeyException | InputRequiredException e) {
+            // 使用日志框架记录异常信息
+            System.err.println("An error occurred while calling the generation service: " + e.getMessage());
+        }
+        System.exit(0);
+    }
+
+}
